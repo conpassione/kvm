794b371006f89a9c3dd707631ad99e4072a12c7d
Renzo Bauen <info@conpassione.ch>
2026-01-08 22:00:11 +0100

    Configuration and Layout of Forms and E-Mails updated

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,3 +16,9 @@ ExtensionManagementUtility::addTypoScriptSetup(
    }
 '
 );
+
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths'][36651] = 'EXT:kvm/Resources/Private/Email/Layouts/';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['templateRootPaths'][36651] = 'EXT:kvm/Resources/Private/Email/Templates/';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['partialRootPaths'][36651] = 'EXT:kvm/Resources/Private/Email/Partials/';
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['kvm']['logoSVG'] = 'EXT:kvm/Resources/Public/Images/Logo.svg';
