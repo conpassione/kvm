4bf840393fa1a1c367ed80f7c6c17a3d2f0babee
Renzo Bauen <info@conpassione.ch>
2026-02-06 16:54:40 +0100

    Form-E-Mail auf CICD KVM umgebaut

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,4 +21,4 @@ $GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths'][36651] = 'EXT:kvm/Resourc
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['templateRootPaths'][36651] = 'EXT:kvm/Resources/Private/Email/Templates/';
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['partialRootPaths'][36651] = 'EXT:kvm/Resources/Private/Email/Partials/';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['kvm']['logoSVG'] = 'EXT:kvm/Resources/Public/Images/Logo.svg';
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['kvm']['logoSVG'] = 'EXT:kvm/Resources/Public/Images/SeitenleisteKVM.png';
